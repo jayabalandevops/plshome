6cafa1a8e9b58f90022bec809b3f28623cd24da5
jayabalandevops <balaniaf@gmail.com>
2019-10-01 00:23:16 +0530

    removed dollar symbal from bStr from app-build.groovy

--- app/build.groovy
+++ app/build.groovy
@@ -3,5 +3,5 @@ import groovy.json.JsonSlurperClassic
 cDelivery = readFile "$wrks/config/cpl.json"
 def jString = new JsonSlurperClassic().parseText(cDelivery)
 def bStr = jString.btool
-bStr = $bStr + " -f appl/pom.xml " + jString.target
+bStr = bStr + " -f appl/pom.xml " + jString.target
 sh "$bStr"
